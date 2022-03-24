a1e73739112a99b059d0367050007917aade26bd
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-03-24 03:52:23 -0400

    on final endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -61,12 +61,59 @@ router.post('/', (req, res) => {
 })
 
 
-router.delete('/:id', (req, res) => {
-    
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The post could not be removed',
+            err: err.message
+        })
+    }
 })
 
 router.put('/:id', (req, res) => {
-    
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: " Please provide title and contents for the post"
+        })
+    } else {
+        Post.findById(req.params.id)
+        .then(stuff => {
+            if (!stuff) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            } else {
+                return Post.update(req.params.id, req.body)
+            }
+        })
+        .then(data => {
+            if (data) {
+                return Post.findById(req.params.id)
+            }
+        })
+        .then(post => {
+            if (post) {
+                res.json(post)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'The post could not be removed',
+                err: err.message
+            })
+        })
+    }
 })
 
 router.get('/:id/messages', (req, res) => {
