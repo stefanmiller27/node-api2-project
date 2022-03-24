35937fe86b27bf2f1dfba6565cec0b9d1c9c48c7
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-03-24 02:30:03 -0400

    hafway mark

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,7 +1,58 @@
 // implement your posts router here
 
 const express = require('express')
-
+const Post = require('./posts-model')
 const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    Post.find()
+        .then(found => {
+            res.json(found)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'The posts information could not be retrieved',
+                err: err.message
+            })
+        })
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+            const post = await Post.findById(req.params.id)
+            if (!post)   {
+                res.status(404).json({
+                  message: "the post with the specified ID does not exist"  
+                })
+            }  else {
+                res.json(post)
+            } 
+    } catch (err) {
+            res.status(500).json({
+            message: 'The post with the specified ID does not exist',
+            err: err.message
+        })
+    }
+})
+
+
+router.post('/', (req, res) => {
+    
+})
+
+
+router.delete('/:id', (req, res) => {
+    
+})
+
+router.put('/:id', (req, res) => {
+    
+})
+
+router.get('/:id/messages', (req, res) => {
+    
+})
+
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require your server and launch it here
 const server = require('./api/server')
 
-const PORT = 9000
+const PORT = 5000
 
 server.listen(PORT, () => {
     console.log('listening on', PORT)
